4f9d6642cd1f98f589d4cedf5fc855a8282f7c9d
duncan <=>
2021-04-22 12:55:54 +0300

    Build and Test Chrome Version, Add Screenshots, and Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Curb Your Timewasting
+
+**Available for Chrome and Firefox.**
+
+![Screenshot of Curb Your Timewasting](https://github.com/mudulo/curbyourtimewasting/raw/master/screenshot.png)
+
+To avoid wasting time on Twitter and YouTube, this extension asks you why you are visiting the sites and then loads a search results page of the site with this reason or query. 
+
+### Chrome:
+
+Download here: https://chrome.google.com/webstore/detail/hn-card/medlpimkoihjpkajkdlkjmphcdgldgkm
+
+### Firefox
+
+Download here: https://addons.mozilla.org/en-US/firefox/addon/hn-card
---
+++ chrome/background.js
@@ -0,0 +1,21 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        
+        var href = new URL(details.url)
+        alert(href);
+        //alert(details.url.hostname + details.url.pathname)
+        if( (href.hostname == "www.youtube.com"  || href.hostname == "youtube.com") && href.pathname == "/" ){
+            return {
+                redirectUrl : chrome.extension.getURL("pages/youtube.html")
+            };
+        } else if( (href.hostname == "www.twitter.com"  || href.hostname == "twitter.com") && href.pathname == "/" ){
+            return {
+                redirectUrl : chrome.extension.getURL("pages/twitter.html")
+            };
+        } else {
+            return
+        }
+    },
+    {urls: ['*://*.youtube.com/*', '*://twitter.com/*'], types: ['main_frame']},
+    ['blocking']
+);
